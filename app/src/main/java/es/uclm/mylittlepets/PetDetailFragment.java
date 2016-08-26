ad1eec9acf944ab8c9071ec794220110e10e23e7
Iván García Herrera <iesivangarcia@gmail.com>
2016-08-26 18:10:17 +0200

    Create PetDetailFragment.java

---
+++ app/src/main/java/es/uclm/mylittlepets/PetDetailFragment.java
@@ -0,0 +1,27 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import es.uclm.mylittlepets.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PetDetailFragment extends Fragment {
+
+
+    public PetDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pet_detail, container, false);
+    }
+
+}
